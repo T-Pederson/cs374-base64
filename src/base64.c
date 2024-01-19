0f68e31dff6b7e3a7584fb19803cae3f7a236329
Tyson <tysonpederson95@gmail.com>
2024-01-18 20:41:49 -0500

    Build base64.c

--- src/base64.c
+++ src/base64.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 #include "gprintf.h"
 
@@ -12,24 +13,90 @@ static char const b64a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                            "abcdefghijklmnopqrstuvwxyz"
                            "0123456789"
                            "+/";
+static char const pad_char = '=';        /* Padding character */
+
+#define TRIBBLE_BIT 3
 
 int
 main(int argc, char *argv[])
 {
-  printf("The base64 alphabet is: %s\n", b64a);
+  // Select input stream
+  int i = 1;
+  char const *filename;
+  FILE *fp;
 
-  /* You can use the gprintf function provided along with this source code to
-   * print debugging messages, which won't show up in your release build.
-   */
-  gprintf("I received %d arguments\n", argc);
-  for (int i = 0; i < argc; ++i) {
-    gprintf("Argument %d is %s\n", i, argv[i]);
+  if (argc < 2) {
+    // No file, read from stdin
+    filename = "-";
+    fp = stdin;
+    goto dfl_stdin;
   }
 
-  assert(0); /* Notice that assertions only affect the debug build; this is
-                because the NDEBUG macro is defined in the command-line of the
-                release build (-DNDEBUG) */
+  for (; i < argc; i++) {
+    filename = argv[i];
 
-  puts("Made it to the end! Goodbye :)");
+    if (strcmp(filename, "-") == 0) {
+      // Filename is "-", read from stdin
+      fp = stdin;
+    } else {
+      // Open the file
+      fp = fopen(filename, "r");
+      if (!fp) err(EXIT_FAILURE, "%s", filename);
+    }
+
+  // loop
+  dfl_stdin:;
+    char buf[BUFSIZ];
+    for (;;) {
+      // read input bytes
+      size_t nr = fread(buf, 1, sizeof buf, fp);
+      if (nr < sizeof buf && ferror(stdin)) err(EXIT_FAILURE, "%s", argv[1]);
+      if (nr == 0) break; // end of file, empty buffer
+      if (nr < sizeof buf) break; // end of file, partial buffer
+
+      // convert input bytes to integer indicies
+      size_t wrap_count = 0;
+      for (size_t i = 0; i < nr; ++i) {
+        // Shift 3 byes into a dword
+        int bytes = 1;
+        unsigned long dword = buf[i];
+
+        // Second byte
+        dword <<= CHAR_BIT;
+        if (++i < nr) {
+          dword |= buf[i];
+          ++bytes;
+        }
+
+        // Third byte
+        dword <<= CHAR_BIT;
+        if (++i < nr) {
+          dword |= buf[i];
+          ++bytes;
+        }
+
+        // convert integer indices to base64 alphabet characters and write output while handling line wrapping
+        for (int j = 0; j < ((3 * CHAR_BIT) / TRIBBLE_BIT); ++j) {
+          if ((j * TRIBBLE_BIT) > (bytes * CHAR_BIT)) {
+            putchar(pad_char);
+          } else {
+            int idx = dword >> (3 * CHAR_BIT - TRIBBLE_BIT);
+            char c = b64a[idx];
+            putchar(c);
+            dword <<= TRIBBLE_BIT; /* Left shift */
+            dword &= 0xffffff;     /* Discard upper bits > 24th position */
+          }
+          ++wrap_count;
+          if (wrap_count == 76) putchar('\n');
+        }
+      }
+    }
+    putchar('\n');
+    if (fp != stdin)
+      fclose(fp);
+  }
+  // cleanup
+  fflush(stdout);
+  if (ferror(stdout)) err(EXIT_FAILURE, "stdout");
   return EXIT_SUCCESS;
 }
